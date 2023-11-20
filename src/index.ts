9fa5612aa54dba91fed3698d97f4730e2f328c5f
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-11-20 12:21:20 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+import express from "express";
+import { config } from "dotenv";
 
-const nome: string = 'Lucas Vinicius Sigoli'; 
+config();
 
-console.log(`Hello, world! ${nome}`);
+const app = express();
+
+const port = process.env.PORT || 50000;
+app.listen(port, () => console.log(`listenning on ports ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});

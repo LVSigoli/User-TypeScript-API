feda6b29b96bee3b1b7f44c805cb5d4f59a5ec85
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-12-17 19:10:20 -0300

    feat: ensure GetUsersMongoRepository gets users from Mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,11 @@
 import { IGetUsersRepository } from "../../controllers/getusers/GetUsersProtocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firtsName: "Lucas",
-        lastName: "Sigoli",
-        email: "lucasviniciussigoli@gmail.com",
-        password: "22121104",
-      },
-    ];
+    const users = await MongoClient.db.collection<Omit<User, "id">>("users").find({}).toArray();
+
+    return users.map(({ _id, ...rest }) => ({ ...rest, id: _id.toHexString() }));
   }
 }
